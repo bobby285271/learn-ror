7058898a6063e9e436d87b738d94b307560be2e9
Bobby Rong <rjl931189261@126.com>
2022-06-02 20:16:27 +0800

    Init welcome/show action and view.
    Tested with http://localhost:3000/page/2

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,4 +3,9 @@ class WelcomeController < ApplicationController
     # This fetches all pages from db.
     @pages = Page.all
   end
+
+  def show
+    # :id will be captured when handling a request
+    @page = Page.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   get 'bobby/welcome/test', to: "welcome#index"
+  get 'page/:id', to: "welcome#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
