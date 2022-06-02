a1c4db63c283ede16b16d5660d59c74fe3bd260c
Bobby Rong <rjl931189261@126.com>
2022-06-02 20:31:25 +0800

    welcome: switch to resources
    Here is the result of `rails routes`:
    
                                      Prefix Verb   URI Pattern                                                                                       Controller#Action
                               welcome_index GET    /welcome(.:format)                                                                                welcome#index
                                             POST   /welcome(.:format)                                                                                welcome#create
                                 new_welcome GET    /welcome/new(.:format)                                                                            welcome#new
                                edit_welcome GET    /welcome/:id/edit(.:format)                                                                       welcome#edit
                                     welcome GET    /welcome/:id(.:format)                                                                            welcome#show
                                             PATCH  /welcome/:id(.:format)                                                                            welcome#update
                                             PUT    /welcome/:id(.:format)                                                                            welcome#update
                                             DELETE /welcome/:id(.:format)                                                                            welcome#destroy
                                        root GET    /                                                                                                 welcome#index
    
    So we don't need to reinvent the wheels.
    
    However, you now need to visit the page details with http://localhost:3000/welcome/1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  get 'bobby/welcome/test', to: "welcome#index"
-  get 'page/:id', to: "welcome#show"
+  resources :welcome
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
