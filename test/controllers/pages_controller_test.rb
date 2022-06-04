527b64ac17e6cca9a7df5bffe323f19389260c87
Bobby Rong <rjl931189261@126.com>
2022-06-04 19:08:42 +0800

    test: reflect the PagesController rename change

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class PageControllerTest < ActionDispatch::IntegrationTest
+class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get page_index_url
     assert_response :success
