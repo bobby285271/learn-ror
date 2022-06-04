7020eff4eadf39642021ad1bc24835cf7ba3d9dd
Bobby Rong <rjl931189261@126.com>
2022-06-04 18:32:00 +0800

    page_controller: init for new page

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -8,4 +8,21 @@ class PageController < ApplicationController
     # :id will be captured when handling a request
     @page = Page.find(params[:id])
   end
+
+  def new
+    # Instantiates a new article, but does not save it
+    @page = Page.new
+  end
+
+  def create
+    # Title and content here are dummy values
+    @page = Page.new(title: "...", content: "...")
+
+    if @page.save
+      redirect_to @page
+    else
+      # https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/422
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
