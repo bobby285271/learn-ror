fb9d2333a5bf8a5570ec858bd947de369a6399e5
Bobby Rong <rjl931189261@126.com>
2022-06-04 18:25:16 +0800

    refactor: rename welcome to page
    Reflect what I am actually doing here.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :welcome
+  resources :page
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'welcome#index'
+  root 'page#index'
 end
