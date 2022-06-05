08657c8492f81bac6c8d4f020a554562464aaa14
Bobby Rong <rjl931189261@126.com>
2022-06-05 21:53:32 +0800

    feat: leave a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+    def create
+        @page = Page.find(params[:page_id])
+        @comment = @page.comments.create(comment_params)
+        redirect_to page_path(@page)
+    end
+    
+    private
+        def comment_params
+            params.require(:comment).permit(:commenter, :content)
+        end
 end
