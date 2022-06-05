ceadfb06f7106d632d41e75db3523d5c46cf3fae
Bobby Rong <rjl931189261@126.com>
2022-06-05 21:42:53 +0800

    config/routes: creates comments as a nested resource within pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :pages
+  resources :pages do
+    resources :comments
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
