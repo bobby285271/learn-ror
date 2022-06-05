ebc308b12a58e4c67d0b943b4323c6f112a402f0
Bobby Rong <rjl931189261@126.com>
2022-06-05 21:40:48 +0800

    models/page: mention there are many comments
    You can retrieve all the comments belonging to a page as an array using @page.comments

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,7 @@
 class Page < ApplicationRecord
+    # To check: Trailing 's'
+    has_many :comments
+
     validates :title, presence: true
     validates :content, presence: true, length: { minimum: 5 }
 end
