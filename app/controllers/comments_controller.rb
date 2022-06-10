5e73909fd01d98476df5f09835bd4713cd774559
Bobby Rong <rjl931189261@126.com>
2022-06-10 23:33:21 +0800

    controllers/comments: permit status

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,6 @@ class CommentsController < ApplicationController
     
     private
         def comment_params
-            params.require(:comment).permit(:commenter, :content)
+            params.require(:comment).permit(:commenter, :content, :status)
         end
 end
