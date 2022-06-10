46e1526595c216be5f63bc70135fa6d42f794e11
Bobby Rong <rjl931189261@126.com>
2022-06-10 23:30:22 +0800

    db: migrate
    Generated by `rails db:migrate`

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_05_133311) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_10_152821) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "content"
     t.integer "page_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["page_id"], name: "index_comments_on_page_id"
   end
 
@@ -25,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_05_133311) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   add_foreign_key "comments", "pages"
