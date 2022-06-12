3aad92a31d0d406a3c48bd09846a150c1a997912
Bobby Rong <rjl931189261@126.com>
2022-06-12 17:48:22 +0800

    models/page: add status

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,4 +4,12 @@ class Page < ApplicationRecord
 
   validates :title, presence: true
   validates :content, presence: true, length: { minimum: 5 }
+
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == 'archived'
+  end
 end
