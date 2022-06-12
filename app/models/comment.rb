1f51f821a8b7421e3350190fe2f2487fcff2d24b
Bobby Rong <rjl931189261@126.com>
2022-06-12 17:48:59 +0800

    models/comment: add status

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
   # Sets up an active record association
   belongs_to :page
+
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == 'archived'
+  end
 end
