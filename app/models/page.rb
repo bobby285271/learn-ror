10e626848073b98de0047219143a9af3e0b2e73d
Bobby Rong <rjl931189261@126.com>
2022-06-12 18:34:38 +0800

    models/page: correctly delete associated objects

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,7 +2,7 @@ class Page < ApplicationRecord
   include Visible
 
   # To check: Trailing 's'
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true
   validates :content, presence: true, length: { minimum: 5 }
