2cbbc3bcea9deaa3af304d01a2d3ae34ba549dd5
Bobby Rong <rjl931189261@126.com>
2022-06-12 17:59:39 +0800

    models/page: remove duplicate logic

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,15 +1,9 @@
 class Page < ApplicationRecord
+  include Visible
+
   # To check: Trailing 's'
   has_many :comments
 
   validates :title, presence: true
   validates :content, presence: true, length: { minimum: 5 }
-
-  VALID_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
 end
