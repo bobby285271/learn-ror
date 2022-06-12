39cd0f277f153082c753faebefc3783d72f63a9a
Bobby Rong <rjl931189261@126.com>
2022-06-12 18:19:40 +0800

    feat: deleting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,13 @@ class CommentsController < ApplicationController
     @comment = @page.comments.create(comment_params)
     redirect_to page_path(@page)
   end
+
+  def destroy
+    @page = Page.find(params[:page_id])
+    @comment = @page.comments.find(params[:id])
+    @comment.destroy
+    redirect_to page_path(@page), status: 303
+  end
     
   private
     def comment_params
