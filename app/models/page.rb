f6ec37f2da01af0992bf73026b55ded98de2be2b
Bobby Rong <rjl931189261@126.com>
2022-06-12 17:47:32 +0800

    trivial: fix indent for page

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,7 @@
 class Page < ApplicationRecord
-    # To check: Trailing 's'
-    has_many :comments
+  # To check: Trailing 's'
+  has_many :comments
 
-    validates :title, presence: true
-    validates :content, presence: true, length: { minimum: 5 }
+  validates :title, presence: true
+  validates :content, presence: true, length: { minimum: 5 }
 end
